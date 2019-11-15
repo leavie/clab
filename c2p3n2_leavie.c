81972852b9c1b58e140c3f607778286a1f54219c
leavie <gola520xxx@gmail.com>
2019-11-16 07:29:01 +0800

    Add files via upload

--- c2p3n2_leavie.c
+++ c2p3n2_leavie.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <wchar.h>
 
-#define STOP 0
-#define PASS 1
+#define STOP '*'
+#define PASS ' '
 
 typedef struct Cell {
 int x;
@@ -10,67 +11,67 @@ int parent;
 } Cell;
 
 int main(void) {
-    int maze[5][5] = {
-        {1,1,1,1,1},
-        {1,1,0,1,1},
-        {1,1,0,1,1},
-        {1,1,0,1,1},
-        {1,1,0,1,1}
+    wchar_t maze[5][5] = {
+        {' ',' ','*',' ',' '},
+        {' ','*',' ',' ',' '},
+        {' ',' ',' ',' ',' '},
+        {' ','*','*',' ',' '},
+        {' ',' ','*',' ',' '}
     };
-    int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
-    Cell queue[25];
+    int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}}; // 代表上下左右 （順序不計）
 
-    int q_start = 0;
-    int q_end = 1;
+    Cell queue[25]; // 隊列
+    int q_start = 0; // 列隊頭部
+    int q_end = 1; // 列隊的尾部
 
-    Cell start_cell = {0,0,-1};
-    Cell curr_cell = start_cell;
-    Cell end_cell = {4,4,-1};
+    Cell start_cell = {0,0,-1}; // 代表迷宮起點的Cell
+    Cell end_cell = {4,4, 9999/*屠龍寶刀點擊就送只要9999 隨便填 因為這個欄位並不會使用到*/}; // 迷宮終點
 
-    int sol = 0;
+    queue[q_start] = start_cell; // 開始尋路前，隊列需要一個起始點
 
-    while (1) {
+  //前置作業
+  //=========分隔線========
+  //計算
 
-    printf("s=%d, e=%d\n", q_start, q_end);
-    if (q_start == q_end) {
-            puts("no way in loop===");
+    Cell curr_cell;
+    while (q_start != q_end) {
+      //printf("s=%d, e=%d\n", q_start, q_end);
+      Cell last_cell = queue[q_end-1];
+      //printf("cx=%d, ex=%d, cy=%d, ey=%d\n",last_cell.x ,end_cell.x , last_cell.y , end_cell.y);
+      if (last_cell.x == end_cell.x && last_cell.y == end_cell.y) { // 當列隊的尾部的Cell的座標相等於迷宮終點 => 找對路徑
+        while(1) { // 反向列印路徑
+          printf("<-(%d,%d)",last_cell.x+1, last_cell.y+1);
+          if (last_cell.parent > -1)
+            last_cell = queue[last_cell.parent];
+          else
             break;
-    }
-
-
-
-        for(int i =0 ;i <4;i++) {
-            int nextY = curr_cell.y+dirs[i][0];
-            int nextX = curr_cell.x+dirs[i][1];
-
-            if (nextY >= 0 && nextY < 5 && nextX >= 0 && nextX < 5) { // fuck1
-
-                int state = maze[nextY][nextX];
-                printf("state=%d\n", state);
-                printf("nextY=%d ,nextX=%d\n", nextY, nextX);
-                if (state == PASS) {
-                    Cell next_cell = {nextX,nextY,q_start};
-                    queue[q_end++] = next_cell;
-                    maze[nextY][nextX] = STOP;
-                }
-            }
         }
-        curr_cell = queue[++q_start];
-                printf("cx=%d, ey=%d, cy=%d, ey=%d\n",queue[q_end-1].x ,end_cell.x , queue[q_end-1].y , end_cell.y);
-    if (queue[q_end-1].x == end_cell.x && queue[q_end-1].y == end_cell.y) {
-            sol = 1;
-            break;
-    }
-    }
-    curr_cell = queue[q_end-1];
-    if (sol) {
-        while(curr_cell.parent > 0) {
-            printf("<-(%d,%d)",curr_cell.x+1, curr_cell.y+1);
-            curr_cell = queue[curr_cell.parent];
-        }
-    }
-    else
-        printf("No Way\n");
+        //printf("%d %d\n", queue[0].x, queue[0].y);
+        return 0;
+      }
 
+      curr_cell = queue[q_start]; // 從列隊頭部得到
+      for(int i =0 ;i <4;i++) { // 列舉出四個方向，產生四個相鄰地點
+          int nextY = curr_cell.y+dirs[i][0]; // y座標
+          int nextX = curr_cell.x+dirs[i][1]; // x座標
+
+          if (nextY >= 0 && nextY < 5 && nextX >= 0 && nextX < 5) { // 判斷有沒有超出迷宮的邊界
+              int state = maze[nextY][nextX]; // 獲取相鄰座標在迷宮中的通過與否狀態
+              //printf("state=%d\n", state);
+              //printf("nextY=%d ,nextX=%d\n", nextY, nextX);
+              if (state == PASS) { // 如果可以通過
+                  Cell next_cell = {nextX,nextY,q_start}; // 將迷宮的兩個相鄰地點的相鄰關係成與包裝成一個Cell記錄起來
+                  queue[q_end++] = next_cell; // 將紀錄好的Cell加入進列隊的尾部
+                  maze[nextY][nextX] = STOP; // 避免迷宮裡的同個Cell通過兩次
+              }
+          }
+      }
+      q_start++;
+    }
+
+    //printf("s=%d, e=%d\n", q_start, q_end);
+    printf("No Way\n");
+
+    return 0;
 
 }
