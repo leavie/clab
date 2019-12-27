f7110faa063e7f7d2bd0ff3ad4d91a56c1eff423
leavie <gola520xxx@gmail.com>
2019-12-27 16:50:46 +0800

    Update palidrome.c

--- palidrome.c
+++ palidrome.c
@@ -8,14 +8,15 @@ while ((ch=getchar())!='\n') {
     freq[ch]++;
 }
 
-int odd_freq = 0;
+int odd_freq = 0; // 奇數出現的次數
 for(int i=0;i<128;i++) {
-    if (isValid(i) && freq[i] % 2 == 1 && (++odd_freq > 1)) {
+    if (isValid(i) && freq[i] % 2 == 1 && (++odd_freq > 1)) { // 奇數出現的次數超過1次, 印出impossible, 結束程式
         puts("impossible.");
         return 0;
     }
 }
 
+// 左至右印出偶數次數的元素每個元素只印一半次數
 for(int i=0;i<128;i++) {
     if (isValid(i) && freq[i] > 0) {
         if(freq[i] % 2 == 0) {
@@ -26,6 +27,7 @@ for(int i=0;i<128;i++) {
     }
 }
 
+// 印出奇數次數的元素
 for(int i=0;i<128;i++) {
     if (isValid(i) && freq[i] > 0) {
         if(freq[i] % 2 == 1) {
@@ -36,6 +38,7 @@ for(int i=0;i<128;i++) {
     }
 }
 
+// 右至左印出偶數次數的元素 每個元素印完另一半
 for(int i=127;i>=0;i--) {
     if (isValid(i) && freq[i] > 0) {
         if(freq[i] % 2 == 0) {
